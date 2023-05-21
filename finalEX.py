c3f51a8434c3eb88fa5e983108a75c52402e32c8
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-05-21 13:09:04 +0300

    Request and answer from openai

--- finalEX.py
+++ finalEX.py
@@ -1,6 +1,27 @@
 from pptx import Presentation
+import openai
+import asyncio
+import collections.abc
 
 
+async def generate_answer_gpt(slides):
+    openai.api_key = KEY
+
+    request = "Hi,could you please summarize the following slides for me?\n"
+    for slide in slides:
+        request += slide
+
+    response = await asyncio.to_thread(openai.ChatCompletion.create,
+                                       model="gpt-3.5-turbo",
+                                       messages=[
+                                           {"role": "system", "content": request},
+                                       ],
+                                       max_tokens=1000)
+
+    answer = response.choices[0].message.content.strip()
+    answer = answer.replace(". ", ".\n")
+
+    return answer
 
 
 def read_presentation(path):
@@ -15,14 +36,15 @@ def read_presentation(path):
         text.append(text_slide)
 
     print(text)
+    return text
 
 
-def main():
-    path = input("please enter a path: ")
-    presentation_text = read_presentation(path)
-
-
+async def main():
+    path_of_pptx = input("please enter a path: ")
+    presentation_text = read_presentation(path_of_pptx)
+    answer = await generate_answer_gpt(presentation_text)
+    print(answer)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
