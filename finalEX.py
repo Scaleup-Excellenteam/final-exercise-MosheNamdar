868b887fc25a6ccd10ef815977b81126d3a57c86
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-05-22 00:25:41 +0300

    with output to a jason file

--- finalEX.py
+++ finalEX.py
@@ -2,6 +2,14 @@ from pptx import Presentation
 import openai
 import asyncio
 import collections.abc
+import json
+from os import path
+
+#KEY =
+
+
+def save_answer_on_json_file(answer, jason_file_name):
+    json.dump(answer, open(jason_file_name, "w"))
 
 
 async def generate_answer_gpt(slides):
@@ -24,8 +32,8 @@ async def generate_answer_gpt(slides):
     return answer
 
 
-def read_presentation(path):
-    file = Presentation(path)
+def read_presentation(pptx_path):
+    file = Presentation(pptx_path)
     text = []
     for slide in file.slides:
         text_slide = ""
@@ -44,6 +52,9 @@ async def main():
     presentation_text = read_presentation(path_of_pptx)
     answer = await generate_answer_gpt(presentation_text)
     print(answer)
+
+    jason_file_name = path.splitext(path_of_pptx)[0] + ".json"
+    save_answer_on_json_file(answer, jason_file_name)
 
 
 if __name__ == '__main__':
