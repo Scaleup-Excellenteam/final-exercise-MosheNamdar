d149745ab54241e0f827a879308b8447fe112820
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-06-13 19:33:36 +0300

    with documentation

--- web_app.py
+++ web_app.py
@@ -21,6 +21,18 @@ if not os.path.exists(UPLOAD):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    Renders the main page for file upload and processes the uploaded file.
+
+    Method:
+    - GET and POST
+
+    Returns:
+    - GET: Renders the 'upload_page.html' template.
+    - POST: Uploads the file, generates a custom filename, saves the file,
+            extracts information from the filename, and renders the 'upload_page.html' template
+            with the extracted information.
+    """
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'File not uploaded'
@@ -45,11 +57,30 @@ def index():
 
 
 def is_valid_file(filename):
+    """
+    Checks if the given filename has a valid extension.
+
+    Parameters:
+    - filename (str): The name of the file.
+
+    Returns:
+    - True if the file has a valid extension.
+    - False otherwise.
+    """
     valid_extensions = {'pptx'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in valid_extensions
 
 
 def generate_custom_filename(file_name):
+    """
+    Generates a custom filename by appending a timestamp and a unique ID to the original filename.
+
+    Parameters:
+    - file_name (str): The original filename.
+
+    Returns:
+    - The custom filename (str).
+    """
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     uid = str(uuid.uuid4().hex)
     filename_without_extension, extension = os.path.splitext(file_name)
@@ -58,6 +89,17 @@ def generate_custom_filename(file_name):
 
 
 def get_status(file_name):
+    """
+    Gets the status of a file based on its name.
+
+    Parameters:
+    - file_name (str): The name of the file.
+
+    Returns:
+    - 'done' if the file exists in the 'outputs' folder as either a .pptx or .json file.
+    - 'pending' if the file exists in the 'pending' folder.
+    - 'not found' if the file does not exist.
+    """
     pptx_filepath = os.path.join(DONE, os.path.splitext(file_name)[0] + '.pptx')
     json_filepath = os.path.join(DONE, os.path.splitext(file_name)[0] + '.json')
 
@@ -72,6 +114,19 @@ def get_status(file_name):
 
 
 def get_details(file_name):
+    """
+    Retrieves details about a file, including the status, original filename, timestamp, and explanation.
+
+    Parameters:
+    - file_name (str): The name of the file.
+
+    Returns:
+    - A dictionary containing the file details:
+        - 'status': The status of the file (str).
+        - 'filename': The original filename (str).
+        - 'timestamp': The formatted timestamp (str).
+        - 'explanation': The explanation data (dict) or None.
+    """
     filename_without_extension, _ = os.path.splitext(file_name)
     timestamp, uid = filename_without_extension.rsplit('_', 2)[-2:]
     original_filename = filename_without_extension.split('_', 1)[0]
@@ -84,6 +139,16 @@ def get_details(file_name):
 
 
 def get_explanation(file_name):
+    """
+    Retrieves the explanation data from a .json file corresponding to the given file name.
+
+    Parameters:
+    - file_name (str): The name of the file.
+
+    Returns:
+    - The explanation data (dict) if the .json file exists and can be loaded successfully.
+    - None otherwise.
+    """
     output_file_path = os.path.join(DONE, os.path.splitext(file_name)[0] + '.json')
 
     try:
@@ -98,6 +163,17 @@ def get_explanation(file_name):
 
 @app.route('/status', methods=['GET', 'POST'])
 def status():
+    """
+    Renders the status page and processes the file details based on the HTTP request method.
+
+    Method:
+    - GET and POST
+
+    Returns:
+    - GET: Renders the 'status.html' template with the file details.
+    - POST: Retrieves the UID from the form data, finds the corresponding file name,
+             retrieves the file details, and renders the 'status.html' template with the file details.
+    """
     if request.method == 'POST':
         uid = request.form.get('uid')
         file_name = find_file_name(uid)
@@ -111,6 +187,16 @@ def status():
 
 
 def find_file_name(uid):
+    """
+    Finds the file name that contains the given UID in its name.
+
+    Parameters:
+    - uid (str): The UID to search for.
+
+    Returns:
+    - The file name (str) if a matching file is found.
+    - None otherwise.
+    """
     folders = ['uploads', 'pending', 'outputs']
     for folder in folders:
         for root, dirs, files in os.walk(folder):
@@ -120,6 +206,16 @@ def find_file_name(uid):
 
 
 def format_timestamp(timestamp):
+    """
+    Formats the timestamp string into a specific format.
+
+    Parameters:
+    - timestamp (str): The timestamp to format.
+
+    Returns:
+    - The formatted timestamp (str) in the format: "YYYY-MM-DD at HH:MM:SS" if successful.
+    - None if an IndexError or TypeError occurs during formatting.
+    """
     try:
         formatted_timestamp = "{}-{}-{} at {}:{}:{}".format(
             timestamp[:4], timestamp[4:6], timestamp[6:8],
